e1a28a599df6b0bc3df07455891d4b92617bc382
ulashbaev <oila.taw@mail.ru>
2025-06-30 10:35:43 +0500

    -

--- Backend/TaskManager/src/main/java/com/epam/learning/mapper/AuthMapper.java
+++
@@ -1,9 +0,0 @@
-package com.epam.learning.mapper;
-
-import com.epam.learning.dto.RegisterRequest;
-import com.epam.learning.entitiy.User;
-import org.mapstruct.Mapper;
-@Mapper(componentModel = "spring")
-public interface AuthMapper {
-    User toEntity(RegisterRequest dto);
-}

d7df00adddc9247b7c675cc7755f7dcd12486e9a
ulashbaev <oila.taw@mail.ru>
2025-06-30 12:11:44 +0500

    ====

--- Backend/TaskManager/src/main/java/com/epam/learning/dto/TaskDto.java
+++ Backend/TaskManager/src/main/java/com/epam/learning/dto/TaskDto.java
@@ -1,7 +1,7 @@
 package com.epam.learning.dto;
 
-import com.epam.learning.enums.Priority;
-import com.epam.learning.enums.Status;
+import com.epam.learning.entitiy.enums.Priority;
+import com.epam.learning.entitiy.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;

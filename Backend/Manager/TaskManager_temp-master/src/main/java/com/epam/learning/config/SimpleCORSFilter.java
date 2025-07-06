426545c0c26a76c7b34b4244e92fd86ff6d8ca9e
Azimkhon <130301061+CleverAbdulaziz@users.noreply.github.com>
2025-07-06 14:56:00 +0500

    Update SimpleCORSFilter.java

--- Backend/Manager/TaskManager_temp-master/src/main/java/com/epam/learning/config/SimpleCORSFilter.java
+++ Backend/Manager/TaskManager_temp-master/src/main/java/com/epam/learning/config/SimpleCORSFilter.java
@@ -3,24 +3,40 @@ package com.epam.learning.config;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class SimpleCORSFilter implements Filter {
 
+  private static final List<String> ALLOWED_ORIGINS = Arrays.asList(
+          "http://localhost:4200",
+          "http://localhost:3000"
+  );
+
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-      throws IOException, ServletException {
+          throws IOException, ServletException {
 
+    HttpServletRequest request = (HttpServletRequest) req;
     HttpServletResponse response = (HttpServletResponse) res;
+    String origin = request.getHeader("Origin");
 
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-    response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "*");
+    if (origin != null && ALLOWED_ORIGINS.contains(origin)) {
+      response.setHeader("Access-Control-Allow-Origin", origin);
+      response.setHeader("Access-Control-Allow-Credentials", "true");
+      response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+      response.setHeader("Access-Control-Allow-Headers", "*");
+      response.setHeader("Access-Control-Max-Age", "3600");
+    }
 
-    chain.doFilter(req, res);
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      chain.doFilter(req, res);
+    }
   }
 }
-
